be7ce159c8169ec983507dc5eb4ad3c9080035c2
BrunoMNDantas <bmndantas@gmail.com>
2018-06-01 12:52:26 +0100

    Configuration class added

---
+++ src/main/java/pt/isel/ngspipes/dsl_core/descriptors/Configuration.java
@@ -0,0 +1,10 @@
+package pt.isel.ngspipes.dsl_core.descriptors;
+
+import pt.isel.ngspipes.dsl_core.descriptors.utils.Serialization;
+
+public class Configuration {
+
+    public static final Serialization.Format DEFAULT_TOOL_SERIALIZATION_FORMAT = Serialization.Format.JSON;
+    public static final Serialization.Format DEFAULT_PIPELINE_SERIALIZATION_FORMAT = Serialization.Format.JSON;
+
+}
