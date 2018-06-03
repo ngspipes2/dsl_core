828d12aab71383356486629e3cce40b94815fb29
BrunoMNDantas <bmndantas@gmail.com>
2018-06-03 20:56:09 +0100

    Github configurations added

--- src/main/java/pt/isel/ngspipes/dsl_core/descriptors/Configuration.java
+++ src/main/java/pt/isel/ngspipes/dsl_core/descriptors/Configuration.java
@@ -7,4 +7,9 @@ public class Configuration {
     public static final Serialization.Format DEFAULT_TOOL_SERIALIZATION_FORMAT = Serialization.Format.JSON;
     public static final Serialization.Format DEFAULT_PIPELINE_SERIALIZATION_FORMAT = Serialization.Format.JSON;
 
+    public static final String GITHUB_FILE_AND_DIRECTORY_NAMES_KEY = "name";
+    public static final String GITHUB_BASE_URI = "https://github.com/";
+    public static final String GITHUB_API_URI = "https://api.github.com/repos/";
+    public static final String GITHUB_RAW_URI = "https://raw.githubusercontent.com/";
+
 }
