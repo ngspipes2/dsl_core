9cd1343bde75cd5b59da9b30f3591790afe8be8a
BrunoMNDantas <bmndantas@gmail.com>
2018-07-05 12:34:16 +0100

    PipelinesRepositoryFactory implemented

--- src/main/java/pt/isel/ngspipes/dsl_core/descriptors/Configuration.java
+++ src/main/java/pt/isel/ngspipes/dsl_core/descriptors/Configuration.java
@@ -1,11 +1,12 @@
 package pt.isel.ngspipes.dsl_core.descriptors;
 
+import pt.isel.ngspipes.dsl_core.descriptors.pipeline.utils.PipelineSerialization;
 import pt.isel.ngspipes.dsl_core.descriptors.utils.Serialization;
 
 public class Configuration {
 
     public static final Serialization.Format DEFAULT_TOOL_SERIALIZATION_FORMAT = Serialization.Format.JSON;
-    public static final Serialization.Format DEFAULT_PIPELINE_SERIALIZATION_FORMAT = Serialization.Format.JSON;
+    public static final PipelineSerialization.Format DEFAULT_PIPELINE_SERIALIZATION_FORMAT = PipelineSerialization.Format.PIPES;
 
     public static final String GITHUB_FILE_AND_DIRECTORY_NAMES_KEY = "name";
     public static final String GITHUB_BASE_URI = "https://github.com/";
--- src/main/java/pt/isel/ngspipes/dsl_core/descriptors/pipeline/PipelinesRepositoryFactory.java
+++ src/main/java/pt/isel/ngspipes/dsl_core/descriptors/pipeline/PipelinesRepositoryFactory.java
@@ -1,5 +1,8 @@
 package pt.isel.ngspipes.dsl_core.descriptors.pipeline;
 
+import pt.isel.ngspipes.dsl_core.descriptors.pipeline.repository.GithubPipelinesRepository;
+import pt.isel.ngspipes.dsl_core.descriptors.pipeline.repository.LocalPipelinesRepository;
+import pt.isel.ngspipes.dsl_core.descriptors.pipeline.repository.ServerPipelinesRepository;
 import pt.isel.ngspipes.pipeline_repository.IPipelinesRepository;
 import pt.isel.ngspipes.pipeline_repository.PipelinesRepositoryException;
 
@@ -12,13 +15,23 @@ public class PipelinesRepositoryFactory {
     @FunctionalInterface
     public interface IPipelinesRepositoryFactory {
 
-        IPipelinesRepository create(String location, Map<String, Object> config);
+        IPipelinesRepository create(String location, Map<String, Object> config) throws PipelinesRepositoryException;
 
     }
 
 
 
-    private static final Collection<IPipelinesRepositoryFactory> FACTORIES = new LinkedList<>();
+    private static final Collection<IPipelinesRepositoryFactory> FACTORIES;
+
+
+
+    static {
+        FACTORIES = new LinkedList<>();
+
+        FACTORIES.add(LocalPipelinesRepository::create);
+        FACTORIES.add(GithubPipelinesRepository::create);
+        FACTORIES.add(ServerPipelinesRepository::create);
+    }
 
 
 
@@ -31,16 +44,39 @@ public class PipelinesRepositoryFactory {
     }
 
     public static IPipelinesRepository create(String location, Map<String, Object> config) throws PipelinesRepositoryException {
-        IPipelinesRepository repository;
+        try {
+            IPipelinesRepository repository = createRepository(location, config);
 
-        for(IPipelinesRepositoryFactory factory : FACTORIES) {
-            repository = factory.create(location, config);
+            if(repository == null)
+                throw new PipelinesRepositoryException("Could not find factory for repository with location:" + location);
+
+            return repository;
+        } catch (PipelinesRepositoryException e) {
+            throw new PipelinesRepositoryException("Could not create instance of IPipelinesRepository for location:" + location, e);
+        }
+    }
+
+    private static IPipelinesRepository createRepository(String location, Map<String, Object> config) throws PipelinesRepositoryException {
+        IPipelinesRepository repository = null;
+        PipelinesRepositoryException ex = null;
+
+        for(PipelinesRepositoryFactory.IPipelinesRepositoryFactory factory : FACTORIES) {
+            try {
+                repository = factory.create(location, config);
+            } catch (PipelinesRepositoryException e) {
+                if(ex == null)
+                    ex = e;
+            }
 
             if(repository != null)
                 return repository;
         }
 
-        throw new PipelinesRepositoryException("Could not find factory to create PipelineRepository for location " + location + "!");
+        if(ex != null)
+            throw ex;
+
+        return null;
     }
+
 
 }
